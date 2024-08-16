cd9c93ac588627b43a9a2c09fc9fa67c50b15ef7
Eljan Najafov <eljan.najafov02@mail.ru>
2024-08-16 19:47:03 +0400

    Initial commit

---
+++ src/main/java/com/en/jewelleryassistantbot/JewelleryAssistantBotApplication.java
@@ -0,0 +1,13 @@
+package com.en.jewelleryassistantbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelleryAssistantBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelleryAssistantBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/en/jewelleryassistantbot/JewelleryAssistantBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.en.jewelleryassistantbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryAssistantBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
